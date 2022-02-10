abace093e37f35304d19fce46d75101429373b2f
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-02-10 23:50:23 +0900

    A+B
    백준 1000번 문제 (A+B)

---
+++ javascript/A+B.js
@@ -0,0 +1,19 @@
+// const fs = require("fs");
+// const inputData = fs.readFileSync(0, "utf8").toString().split(" ");
+
+// const A = parseInt(inputData[0]);
+// const B = parseInt(inputData[1]);
+
+// console.log(A + B);
+
+// let fs = require("fs");
+// let input = fs.readFileSync(0, "utf8").toString().split(" ");
+
+// let num = Number(input);
+
+// for (let i = 1; i <= num; i++) {
+//   console.log(i);
+// }
+
+// 백준에선 경로를 "/dev/stdin" 이렇게 지정해주어야 한다.
+(v = require("fs").readFileSync("/dev/stdin")), console.log(v[0] + v[2] - 96);
