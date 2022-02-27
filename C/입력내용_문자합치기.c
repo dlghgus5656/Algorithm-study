76a790a0d429e3f30500077cbab3140dd89c6f63
dlghgus5656 <82634597+dlghgus5656@users.noreply.github.com>
2022-02-27 20:16:15 +0900

    baekjoon
    10926번 문제
    첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.

---
+++ C/입력내용_문자합치기.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	char id[50];
+	scanf("%s", id);
+	printf("%s?\?!", id);
+
+	return 0;
+}
---
+++ java/입력내용_문자합치기.java
@@ -0,0 +1,7 @@
+import java.util.Scanner; 
+
+public class 입력내용_문자합치기 { 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        String id = in.next(); 
+        System.out.println(id + "??!"); } }
